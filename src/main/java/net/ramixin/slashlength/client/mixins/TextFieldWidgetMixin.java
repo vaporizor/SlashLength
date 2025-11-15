3e561ac5e3860764b272720e6e819bd734718e9c
vaporizor <159073161+vaporizor@users.noreply.github.com>
2025-11-15 10:52:26 +0300

    improved command detection method

--- src/main/java/net/ramixin/slashlength/client/mixins/TextFieldWidgetMixin.java
+++ src/main/java/net/ramixin/slashlength/client/mixins/TextFieldWidgetMixin.java
@@ -13,14 +13,22 @@ import org.spongepowered.asm.mixin.injection.At;
 public abstract class TextFieldWidgetMixin implements TextFieldWidgetDuck {
 
     @Shadow public abstract void setMaxLength(int maxLength);
+    @Shadow private int selectionStart;
+    @Shadow private int selectionEnd;
 
     @Unique
     private boolean isChatBox = false;
 
     @WrapOperation(method = "keyPressed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/TextFieldWidget;write(Ljava/lang/String;)V", ordinal = 0))
     private void preventTruncationIfPastingCommand(TextFieldWidget instance, String text, Operation<Void> original) {
-        if(isChatBox && text.startsWith("/")) setMaxLength(Integer.MAX_VALUE);
-        else setMaxLength(256);
+        boolean isCommand = false;
+        if(isChatBox) {
+            if (Math.min(Math.min(this.selectionStart, this.selectionEnd), instance.getCursor()) > 0) {
+                isCommand = instance.getText().startsWith("/");
+            } else isCommand = text.startsWith("/");
+        }
+        if (isCommand) instance.setMaxLength(Integer.MAX_VALUE);
+        else this.setMaxLength(256);
         original.call(instance, text);
     }
 
