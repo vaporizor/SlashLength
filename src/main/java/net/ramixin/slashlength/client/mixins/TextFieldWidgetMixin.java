5e2496aaa838a0d3fbcefef4a1ea2bff3f4ca695
vaporizor <159073161+vaporizor@users.noreply.github.com>
2025-11-16 01:24:27 +0300

    Fix pasting into a command (#4) (vaporizor)

--- src/main/java/net/ramixin/slashlength/client/mixins/TextFieldWidgetMixin.java
+++ src/main/java/net/ramixin/slashlength/client/mixins/TextFieldWidgetMixin.java
@@ -12,15 +12,22 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(TextFieldWidget.class)
 public abstract class TextFieldWidgetMixin implements TextFieldWidgetDuck {
 
-    @Shadow public abstract void setMaxLength(int maxLength);
+    @Shadow private int selectionStart;
+    @Shadow private int selectionEnd;
 
     @Unique
     private boolean isChatBox = false;
 
     @WrapOperation(method = "keyPressed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/TextFieldWidget;write(Ljava/lang/String;)V", ordinal = 0))
     private void preventTruncationIfPastingCommand(TextFieldWidget instance, String text, Operation<Void> original) {
-        if(isChatBox && text.startsWith("/")) setMaxLength(Integer.MAX_VALUE);
-        else setMaxLength(256);
+        boolean isCommand = false;
+        if(isChatBox) {
+            if (Math.min(Math.min(this.selectionStart, this.selectionEnd), instance.getCursor()) > 0) {
+                isCommand = instance.getText().startsWith("/");
+            } else isCommand = text.startsWith("/");
+        }
+        if (isCommand) instance.setMaxLength(Integer.MAX_VALUE);
+        else instance.setMaxLength(256);
         original.call(instance, text);
     }
 
